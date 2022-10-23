869ab084d00b95b37606d3196efe956854b88afd
unknown <bilalabdulg@gmail.com>
2022-10-23 14:36:37 -0400

    Time out ending

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -10,16 +10,25 @@ public class Timer : MonoBehaviour
     public TextMeshProUGUI timer;
 
     private float _timeRemaining;
+    private bool _tick;
     
     void Start()
     {
         _timeRemaining = timeToSolve;
+        _tick = true;
     }
     
     void Update()
     {
+        if(!_tick) return;
+        
         _timeRemaining -= Time.deltaTime * timerSpeed;
         _timeRemaining = Mathf.Clamp(_timeRemaining, 0, timeToSolve);
         timer.text = "Time remaining: " + (int)_timeRemaining/60 + ":" + ((int)_timeRemaining % 60 < 10 ? 0 + "" + (int)_timeRemaining % 60 : (int)_timeRemaining % 60);
+
+        if (_timeRemaining != 0) return;
+        
+        SceneSwitcher.Fade("OutOfTime");
+        _tick = false;
     }
 }
